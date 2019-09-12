b110aeaa8d15f0d76c496946018e422ae3507ee3
pcf26536 <muffwaindan@gmail.com>
2019-09-12 15:54:43 +0300

    [ch #168166947] update components layout

--- src/eventHandlers/menuToggle.js
+++ src/eventHandlers/menuToggle.js
@@ -1,7 +1,6 @@
 import overlayAction from "./overlayActions";
 
 const menuToggle = evt => {
-  evt.preventDefault();
   const sideBar = document.getElementsByClassName('menu')[0];
   overlayAction(sideBar);
 };
--- src/eventHandlers/signUpModal.js
+++ src/eventHandlers/signUpModal.js
@@ -1,7 +1,6 @@
 import overlayAction from "./overlayActions";
 
 const signUpHandler = evt => {
-  evt.preventDefault();
   const signUpModal = document.getElementsByClassName('sign-up-modal')[0];
   overlayAction(signUpModal);
 };
