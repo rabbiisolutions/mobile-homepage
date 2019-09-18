99fa8e722456dbf0e30016a5ebcbe5e1294b4cb5
pcf26536 <muffwaindan@gmail.com>
2019-09-18 10:03:01 +0300

    [ch #168166947] update more option to teecha steps

--- src/components/sub-items/Step.js
+++ src/components/sub-items/Step.js
@@ -1,12 +1,22 @@
 import React from "react";
-
+import moreHandler from "../../events/moreHandler";
 
 const Step = (props) =>  {
+  let description = null;
+  if (typeof props.description == "string") {
+    description = <div className="step-description">{props.description}</div>;
+  } else if (typeof props.description == "object") {
+    description = <div className="step-description">
+                    <span>{props.description[0]}</span>
+                    <span className="more hidden">{props.description[1]}</span>
+                    <span className="dots" onClick={e => moreHandler(e)}>&nbsp;more...</span>
+                  </div>;
+  }
   return (
       <div className="step" key={props.stepNumber}>
         <div className="step-number">Step {props.stepNumber}</div>
         <div className="step-title">&middot; {props.title}</div>
-        <div className="step-description">{props.description}</div>
+        {description}
       </div>
   );
 };
