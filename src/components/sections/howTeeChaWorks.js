cda48cb0e4ce4e63812aa0b014e945b9353b8cb1
pcf26536 <muffwaindan@gmail.com>
2019-11-27 23:39:16 +0300

    [ft #170006319] add down arrow component and icon imports

--- src/components/sections/howTeeChaWorks.js
+++ src/components/sections/howTeeChaWorks.js
@@ -1,9 +1,24 @@
 import TeeChaSteps from "../../constants/TeeChaSteps";
-import HowTo from "../sub-items/howTo";
+import CircleWidget from "../sub-items/CircleWidget";
+import signup from "../../icons/teecha/application.svg"
+import proInfo from "../../icons/teecha/resume.svg"
+import studInfo from "../../icons/teecha/resume (1).svg"
+import screen from "../../icons/teecha/time.svg"
+import gets from "../../icons/teecha/deal.svg"
+import verification from "../../icons/teecha/verified-account.svg"
+import placement from "../../icons/teecha/recruitment.svg"
+import start from "../../icons/teecha/start-line.svg"
+import evaluation from "../../icons/teecha/evaluation.svg"
+import counselling from "../../icons/teecha/mentor.svg"
+import right from "../../icons/teecha/fa-solid_long-arrow-alt-downright-small.svg"
+import left from "../../icons/teecha/fa-solid_long-arrow-alt-downleft-small.svg"
+import dotted from "../../icons/teecha/Arrow 13dotted-small.svg"
+import Icon from "../utils/icon";
+import DownArrow from "../utils/DownArrow";
 
 const HowTeeChaWorks = () => {
   return (
-      HowTo(TeeChaSteps, 'how-teecha-works')
+
   );
 };
 
---
+++ src/components/utils/DownArrow.js
@@ -0,0 +1,12 @@
+import down from "../../icons/teecha/fa-solid_long-arrow-alt-downdown-solid.svg";
+import Icon from "./icon";
+import React from "react";
+
+
+const DownArrow = () => {
+  return (
+      <Icon src={down} width={1.25} height={1.875} className={'down-arrow'}/>
+  )
+};
+
+export default DownArrow;
