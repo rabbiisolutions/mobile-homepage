4834734f3e10ea987a21439c86b7c4c794f1a201
pcf26536 <muffwaindan@gmail.com>
2019-11-30 13:00:24 +0300

    [ft #170006319] update sign up modal text and style

--- src/components/utils/signUpModal.js
+++ src/components/utils/signUpModal.js
@@ -7,18 +7,26 @@ const SignUpModal = (props) => {
       <div className={props.className}>
         <div className={"close-icon " + props.status} onClick={e => signUpHandler(e)}>&#10006;</div>
         <div className="sign-up-text">{props.signUpText}</div>
-        <div className="user-title">Clients/Parents</div>
-        <div id="client-btns">
-          <ButtonLink link="https://forms.gle/y7KTkMDtYCtAywDu6" className="clients-btn" value="Get a Tutor"/>
-          <ButtonLink link="https://forms.gle/3zg3FSExEBdBPHd39" className="clients-btn" value="Referral"/>
-        </div>
+        <section>
+          <div className="user-title">
+            Sign up as a Client/Parent to get a tutor/Teacher
+          </div>
+          <div id="client-btns">
+            <ButtonLink link="https://forms.gle/y7KTkMDtYCtAywDu6" className="clients-btn" value="Get a Tutor"/>
+            <ButtonLink link="https://forms.gle/3zg3FSExEBdBPHd39" className="clients-btn" value="Referral"/>
+          </div>
+        </section>
         <hr className="sign-up-divider"/>
-        <div className="user-title">Tutors</div>
-        <div id="tutor-btns">
-          <ButtonLink link="https://forms.gle/LerPUYib9EJ3yMxr7" className="tutors-btn" value="Non TSC Tutor"/>
-          <ButtonLink link="https://forms.gle/ZU8GZrYVPWenL4r19" className="tutors-btn" value="TSC Tutor"/>
-          <ButtonLink link="https://forms.gle/CmgUyZCWrnLgsqnC9" className="tutors-btn" value="Skills Tutor"/>
-        </div>
+        <section>
+          <div className="user-title">
+            Register to be a Tutor/Teacher in your specific category
+          </div>
+          <div id="tutor-btns">
+            <ButtonLink link="https://forms.gle/LerPUYib9EJ3yMxr7" className="tutors-btn" value="Non TSC Tutor"/>
+            <ButtonLink link="https://forms.gle/ZU8GZrYVPWenL4r19" className="tutors-btn" value="TSC Tutor"/>
+            <ButtonLink link="https://forms.gle/CmgUyZCWrnLgsqnC9" className="tutors-btn" value="Skills Tutor"/>
+          </div>
+        </section>
         <div className="msg">If you face difficulty in registering call, text or whatsapp on 0772 915 361</div>
       </div>
   );
