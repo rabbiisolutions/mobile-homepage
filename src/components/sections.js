b111efb316e26df87eb1ff1c2a65ad22f41c38e6
pcf26536 <muffwaindan@gmail.com>
2019-12-10 16:58:37 +0300

    [ft #170206822] add webp helper

--- src/components/sections.js
+++ src/components/sections.js
@@ -1,10 +1,18 @@
 import React from "react";
 import SectionsAccordion from "./sectionsAccordion";
-
+import ImageWebp from "../helpers/ImageWebp";
+import landingJ from "../images/jpeg/landing.jpg";
+import landingW from "../images/webp/landing.webp";
 
 const Sections = () => {
   return (
       <div className="sections">
+        <ImageWebp
+            src={landingJ}
+            srcWebp={landingW}
+            width={200}
+            height={200}
+        />
         <div id="sections-title">Sections</div>
         <div id="sections-lead">Tap a section to expand or collapse it</div>
         {SectionsAccordion()}
