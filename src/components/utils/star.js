a2ad08176cd6b67c103918a1fd5c6fac8133ae92
pcf26536 <muffwaindan@gmail.com>
2019-12-11 23:18:37 +0300

    [ft #170206822] add featured tutors styling

--- src/components/utils/star.js
+++ src/components/utils/star.js
@@ -4,7 +4,7 @@ import star from "../../icons/star-solid.svg"
 
 
 const Star = (props) => {
-  return <Icon src={star} className={"star " + props.shade } height={props.height} key={props.key}/>;
+  return <Icon src={star} className={"star " + props.shade } height={props.height} units={'vw'} key={props.key}/>;
 };
 
 export default Star;
--- src/constants/featuredTutors.js
+++ src/constants/featuredTutors.js
@@ -5,16 +5,17 @@ const featuredTutors = [
     name: 'Miss Mutai Sharon', description: 'Masters Student, Mentor, 2 years of experience', key: "d1",
     stars: {
       shade: [texts.black, texts.black, texts.black, texts.black, texts.grey], key: 1
-    }, comment: 'I am a passionate mathematics and physics teacher.I am currently a masters student ' +
+    }, comment: ['I am a passionate mathematics and physics teacher.I am currently a masters' +
+    ' student ' +
         'at The University of Nairobi, pursuing MSc in Applied mathematics.Teaching for me is a' +
-        ' Calling and helping students maximize their ability in learning. I not only teach, but' +
-        ' mentor my students .I am strongly attached to my students and that gives me a chance to' +
+        ' Calling and helping students maximize their ability in learning.', ' I not only teach,' +
+        ' but mentor my students .I am strongly attached to my students and that gives me a chance to' +
         ' deliver to the maximum.\n I have taught for two years in various schools in Nairobi,' +
-        ' which includes St.Annes Girls and Nairobi School.\n',
+        ' which includes St.Annes Girls and Nairobi School.\n'],
     areas: 'Secondary Mathematics and Physics', url: 'https://www.facebook.com/mutai.sharo',
   },
   {
-    name: 'Mr. Calvins Okello', description: '6 years of experience, National Examiner, IT' +
+    name: 'Mr. Calvin Okello', description: '6 years of experience, National Examiner, IT' +
         ' Educationist', key: "d3",
     stars: {
       shade: [texts.black, texts.black, texts.black, texts.black, texts.grey], key: 2
