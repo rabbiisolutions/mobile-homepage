90354cf1a0b364662d91a37ff4a781f93ddfc224
pcf26536 <muffwaindan@gmail.com>
2020-01-22 17:23:26 +0300

    [ft #170814113] add reviews slide show

--- src/components/composite/Review.jsx
+++ src/components/composite/Review.jsx
@@ -5,10 +5,13 @@ const Review = (props) => {
   return (
       <div className="review" key={props.key}>
         <div className="reviewer">
-          <span>{props.name}</span>,&nbsp;<span>{props.description}</span>
+          <span>{props.name}</span>
+        </div>
+        <div className="comment">{props.comment}</div>
+        <div className="desc">
+          <span>{props.description}</span>
         </div>
         <div className="stars">{renderStars({shade: props.stars.shade, height: 4, key: props.stars.key})}</div>
-        <div className="comment">{props.comment}</div>
       </div>
   );
 };
--- src/components/sub-sections/Reviews.jsx
+++ src/components/sub-sections/Reviews.jsx
@@ -1,23 +1,72 @@
+import React from 'react';
+import pilotJpeg from "../../images/reviews/slide-1.jpg";
+import pilotWebp from "../../images/reviews/slide-1.webp";
+import engineerJpeg from "../../images/reviews/slide-2.jpg";
+import engineerWebp from "../../images/reviews/slide-2.webp";
+import saxJpeg from "../../images/reviews/slide-3.jpg";
+import saxWebp from "../../images/reviews/slide-3.webp";
+import photographyJpeg from "../../images/reviews/slide-4.jpg";
+import photographyWebp from "../../images/reviews/slide-4.webp";
+import ImageWebp from "../../helpers/ImageWebp";
 import clientReviews from "../../constants/reviews";
 import Review from "../composite/Review";
 import Accordion from "../basic/Accordion";
+import makeCarousel from 'react-reveal/makeCarousel';
+import Slide from 'react-reveal/Slide';
 
+
+const fadeImages = [
+  {jpeg: pilotJpeg, webp: pilotWebp},
+  {jpeg: engineerJpeg, webp: engineerWebp},
+  {jpeg: saxJpeg, webp: saxWebp},
+  {jpeg: photographyJpeg, webp: photographyWebp},
+];
 
 const Reviews = () => {
-  const reviews = [];
-  reviews.push(
-      Review(clientReviews[0]),
-      Review(clientReviews[1]),
-      Review(clientReviews[2]),
-      Review(clientReviews[3]),
+  const CarouselUI = ({ position, total, handleClick, children }) => (
+      <section id={'reviews-cont'}>
+        <div id={'dots'}>
+          {Array(...Array(total)).map( (val, index) =>
+              <span className={'dot'} key={index} onClick={handleClick} data-position={index}>
+                {index === position ? '● ' : '○ ' }
+              </span>
+          )}
+        </div>
+        <div id={'children'}>
+          {children}
+        </div>
+      </section>
   );
-  return (
-          Accordion.panel(
-              reviews.map((review) => (
-                  review
-              )), 'reviews'
-          )
-  )
+  const Carousel = makeCarousel(CarouselUI);
+
+  return Accordion.panel(
+      <Carousel>
+        <Slide right>
+          <div>
+            <ImageWebp src={fadeImages[2].jpeg} srcWebp={fadeImages[2].webp} className={'review-image'}/>
+            {Review(clientReviews[0])}
+          </div>
+        </Slide>
+        <Slide right>
+          <div>
+            <ImageWebp src={fadeImages[0].jpeg} srcWebp={fadeImages[0].webp} className={'review-image'}/>
+            {Review(clientReviews[1])}
+          </div>
+        </Slide>
+        <Slide right>
+          <div>
+            <ImageWebp src={fadeImages[3].jpeg} srcWebp={fadeImages[3].webp} className={'review-image'}/>
+            {Review(clientReviews[2])}
+          </div>
+        </Slide>
+        <Slide right>
+          <div>
+            <ImageWebp src={fadeImages[1].jpeg} srcWebp={fadeImages[1].webp} className={'review-image'}/>
+            {Review(clientReviews[3])}
+          </div>
+        </Slide>
+      </Carousel>
+  );
 };
 
 export default Reviews;
