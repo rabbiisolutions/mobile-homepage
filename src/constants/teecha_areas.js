88f6e2a797f66f044cafad5019cab33bc39f39dd
pcf26536 <muffwaindan@gmail.com>
2020-01-23 17:59:28 +0300

    [ft #170814113] add google audit suggestions

--- src/constants/teecha_areas.js
+++ src/constants/teecha_areas.js
@@ -5,26 +5,27 @@ const teeChaAreas = {
   ],
   language: [
     {key: 'a1', text: 'Chinese'}, {key: 'a2', text: 'French'},
-    {key: 'a3', text: 'German'}
+    {key: 'a3', text: 'German etc'}
   ],
   sports: [
     {key: 'b1', text: 'Chess'}, {key: 'b2', text: 'Swimming'},
-    {key: 'b3', text: 'Skating'}
+    {key: 'b3', text: 'Skating...etc'}
   ],
   international: [
-    {key: 'c1', text: 'IGCSE'}, {key: 'c2', text: 'American'}, {key: 'c3', text: 'IB etc'}
+    {key: 'c1', text: 'IGCSE'}, {key: 'c2', text: 'American'}, {key: 'c3', text: 'IB...etc'}
   ],
   tests: [
-    {key: 'd1', text: 'CPA exams'}, {key: 'd2', text: 'CIFA exams'}, {key: 'd3', text: 'SAT'}, {key: 'd4', text: 'ACT'}, {key: 'd5', text: 'GMAT'}
+    {key: 'd1', text: 'CPA exams'}, {key: 'd2', text: 'CIFA exams'}, {key: 'd3', text: 'SAT'},
+    {key: 'd4', text: 'ACT'}, {key: 'd5', text: 'Statistics...etc'}
   ],
   hobbies: [
     {key: 'e4', text: 'Photography'}, {key: 'e5', text: 'Dance'},
     {key: 'e6', text: 'Music'}, {key: 'e7', text: 'Art'}, {key: 'e8', text: 'Vocals'},
-    {key: 'e9', text: 'Dj etc'}
+    {key: 'e9', text: 'Dj...etc'}
   ],
   specialNeeds: [
     {key: 'f4', text: 'Physical'}, {key: 'f5', text: 'Developmental'},
-    {key: 'f6', text: 'Behavioural/Emotional'}, {key: 'f7', text: 'Sensory/Impaired'}
+    {key: 'f6', text: 'Behavioural/Emotional'}, {key: 'f7', text: 'Sensory/Impaired...etc'}
   ]
 };
 
